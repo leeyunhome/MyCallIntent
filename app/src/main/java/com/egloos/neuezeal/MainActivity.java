768de6d04fab10ce1639a61f224117820705ad26
leeyunhome <leeyunhome@daum.net>
2020-03-02 15:28:56 +0900

    Initial commit

---
+++ app/src/main/java/com/egloos/neuezeal/MainActivity.java
@@ -0,0 +1,34 @@
+package com.egloos.neuezeal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+                startActivity(intent);
+
+//                Intent intent = new Intent();
+//                ComponentName name = new ComponentName("com.egloos.neuezeal",
+//                        "org.egloos.neuezeal.MenuActivity");
+//                intent.setComponent(name);
+//                startActivityForResult(intent, 101);
+            }
+        });
+    }
+}
